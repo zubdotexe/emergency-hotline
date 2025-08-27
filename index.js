9d9d2afcc2fc67c162f1c7957993d5e9dcb7387b
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-08-27 22:46:42 +0600

    implemented call history functionality
    - This displays call history on UI
    - Users can remove call history using the "Clear" button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const favList = document.getElementsByClassName("like-it");
 const favCounter = document.querySelector("#heart-counter");
 
+// heart cards
 let temp = parseInt(favCounter.innerHTML);
 for(let i = 0; i < favList.length; i++) {
     const currentElem = favList[i];
@@ -22,19 +23,33 @@ for(let i = 0; i < favList.length; i++) {
 
 const callBtns = document.querySelectorAll(".call-btn");
 const coins = document.getElementById("coin-counter");
+const callHistContainer = document.getElementById("call-hist-container");
 
+function addCall(serviceName, serviceNum, callTime) {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = `
+        <div class="bg-[#FEF7FF] mb-2 p-4 rounded-md flex flex-wrap gap-2 justify-between items-center">
+            <div class="flex flex-col flex-wrap w-[60%]">
+                <h2 class="font-bold inter-font">${serviceName}</h2>
+                <p class="text-gray-500">${serviceNum}</p>
+            </div>
+            <p class="text-gray-600 font-semibold">${callTime}</p>
+        </div>
+    `
 
+    callHistContainer.appendChild(tempDiv);
+}
+
+// add history
 for(let i = 0; i < callBtns.length; i++) {
     btn = callBtns[i];
     
     btn.addEventListener("click", (e) => {
         let temp = parseInt(coins.innerText);
-        console.log('temp', temp);
-        console.log('', e.target.parentNode.parentNode);
+        
         const cardElem = e.target.parentNode.parentNode;
         const serviceName = cardElem.querySelectorAll("div")[0].querySelector("h2").innerText;
         const serviceNum = cardElem.querySelectorAll("div")[1].querySelector("p").innerText;
-        console.log('', serviceNum);
         
         
         if(temp < 20) {
@@ -44,6 +59,13 @@ for(let i = 0; i < callBtns.length; i++) {
             alert(`📞 Calling ${serviceName} ${serviceNum}...`);
             temp -= 20;
             coins.innerText = temp;
+            let callTime = new Date().toLocaleTimeString();
+            addCall(serviceName, serviceNum, callTime);
         }
     })
 }
+
+// clear history
+document.getElementById("clear-hist-btn").addEventListener("click", (e) => {
+    callHistContainer.innerHTML = "";
+})
